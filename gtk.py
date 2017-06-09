e5f5dcef72b8e3295b61d35dd197e156ac1716ce
pzl <dan@panzarel.la>
2017-06-09 14:19:23 -0400

    alignment, fancy text, headings

--- gtk.py
+++ gtk.py
@@ -26,7 +26,7 @@ def url_to_icon(url,size=100):
 def newLabel(text):
 	label = Gtk.Label()
 	label.set_justify(Gtk.Justification.LEFT)
-	label.set_text(text)
+	label.set_markup(text)
 	return label
 
 
@@ -46,69 +46,74 @@ def show_info(student):
 	student_icon = Gtk.Image.new_from_pixbuf(url_to_icon(student.photo_url))
 
 	nameBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
-	nameBox.pack_start(newLabel(f"{student.first_name} {student.last_name}"), True, True, 0)
-	nameBox.pack_start(newLabel("checked in" if student.checked_in else "checked out"), True, True, 0)
+	nameBox.pack_start(newLabel(f"<span font='Lato Medium 22'>{student.first_name} {student.last_name}</span>"), False, True, 5)
+	nameBox.pack_start(newLabel("<span font='Lato Light 11'>checked in</span>" if student.checked_in else "<span font='Lato Light 11'>checked out</span>"), False, True, 0)
 
-	idBox.pack_start(student_icon, True, True, 0)
-	idBox.pack_start(nameBox, True, True, 0)
+	idBox.pack_start(student_icon, False, True, 10)
+	idBox.pack_start(nameBox, False, True, 0)
 
-	info.pack_start(idBox, True, True, 0)
+	info.pack_start(idBox, False, True, 0)
 
 
 	# Current Room
 	if student.current_room:
-		roomBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=20)
-		roomBox.pack_start(newLabel(f"Room: {student.current_room.name}"),True,True,0)
+		roomBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+		roomNameBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
+
+		roomNameBox.pack_start(newLabel("<span font='Lato 16'>Room:</span>"),False,False,30)
+		roomNameBox.pack_start(newLabel(f"<span font='Lato 13' underline='single'>{student.current_room.name}</span>"),False,False,0)
+		roomBox.pack_start(roomNameBox,False,False,0)
 
 		teachBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing=0)
 		for t in student.current_room.teachers:
-			tbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL,spacing=2)
+			trow = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL,spacing=2)
 			timg = Gtk.Image.new_from_pixbuf(url_to_icon(t.photo_url,50))
-			tbox.pack_start(timg, True, True, 0)
-			tbox.pack_start(newLabel(str(t)),True,True,0)
-			teachBox.pack_start(tbox, True, True, 0)
+			trow.pack_start(timg, False, True, 10)
+			trow.pack_start(newLabel(f"<span font='Lato Light 11'>{t}</span>"),False,True,0)
+			teachBox.pack_start(trow, False, True, 0)
 
-		roomBox.pack_start(teachBox, True, True, 0)
-		info.pack_start(roomBox, True, True, 0)
+		roomBox.pack_start(teachBox, False, True, 0)
+		info.pack_start(roomBox, False, True, 0)
 
 	# Daily Sheet
 
-	mealsBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=15)
+	info.pack_start(newLabel("<span font='Lato Medium 16'>Meals</span>"),False,False,0)
+	mealsBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
 	for meal in student.info.meals:
 		mealRow = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
-		mealRow.pack_start(newLabel(meal.time),True,True,0)
+		mealRow.pack_start(newLabel(f"<span font='Lato 14'>{meal.time}</span>"),False,True,15)
 
 		foodBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 		for food in meal.foods:
-			foodBox.pack_start(newLabel(str(food)), True, True, 0)
-		mealRow.pack_start(foodBox, True, True, 0)
+			foodBox.pack_start(newLabel(f"<span font='Lato Light 14'>{food}</span>"), False, True, 0)
+		mealRow.pack_start(foodBox, False, True, 0)
 
-		mealsBox.pack_start(mealRow, True, True, 0)
-	info.pack_start(mealsBox, True, True, 0)
+		mealsBox.pack_start(mealRow, False, True, 0)
+	info.pack_start(mealsBox, False, True, 0)
 
+	info.pack_start(newLabel("<span font='Lato Medium 16'>Naps</span>"),False,False,0)
 	napBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
 	for nap in student.info.naps:
 		napRow = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=5)
-		napRow.pack_start(newLabel(f"Start: {nap.start_time}"), True, True, 0)
-		napRow.pack_start(newLabel(f"End: {nap.end_time}"), True, True, 0)
-		napRow.pack_start(newLabel("Duration: %dm %d sec" % divmod(nap.duration,60)), True, True, 0)
+		napRow.pack_start(newLabel(f"<span font='Lato Light 18'>{nap.start_time} - </span>"), True, True, 0)
+		napRow.pack_start(newLabel(f"<span font='Lato Light 18'>{nap.end_time}</span>"), True, True, 0)
+		napRow.pack_start(newLabel("<span font='Lato Medium 16'>Duration: %dm %d sec</span>" % divmod(nap.duration,60)), True, True, 0)
 
 		napBox.pack_start(napRow,True,True,0)
-	info.pack_start(napBox,True,True,0)
+	if not student.info.naps:
+		napBox.pack_start(newLabel("<span font='Lato Light 13'>None</span>"),True,True,0)
+	info.pack_start(napBox,False,True,0)
 
+	info.pack_start(newLabel("<span font='Lato Medium 16'>Bathroom</span>"),False,False,0)
 	pottyBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
 	for potty in student.info.bathroom_visits:
 		pottyRow = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=5)
-		pottyRow.pack_start(newLabel(f"Bathroom visit: {potty.type}"), True, True, 0)
-		pottyRow.pack_start(newLabel(f"At: {potty.time}"), True, True, 0)
+		pottyRow.pack_start(newLabel(f"<span font='Lato 14'>{potty.time}</span>"), False, True, 15)
+		pottyRow.pack_start(newLabel(f"<span font='Lato Light 12'>{potty.type}</span>"), False, False, 0)
+		pottyRow.pack_start(newLabel( "<span font='Lato Light 13'>" + ", ".join(potty.diaper_type) + "</span>" ), False, True, 0)
 
-		types = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-		for t in potty.diaper_type:
-			types.pack_start(newLabel(t),True,True,0)
-		pottyRow.pack_start(types, True, True, 0)
-
-		pottyBox.pack_start(pottyRow,True,True,0)
-	info.pack_start(pottyBox,True,True,0)
+		pottyBox.pack_start(pottyRow,False,True,0)
+	info.pack_start(pottyBox,False,True,0)
 
 
 	layout.pack_start(info, True, True, 0)
